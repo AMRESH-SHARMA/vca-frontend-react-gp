25a9286547618a3f66ebe5d8851ac41f71783654
AMRESH-SHARMA <amreshaiio@gmail.com>
2023-08-22 13:13:57 +0530

    pages name

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import Footer from '../../components/common/footer/Footer'
+import Header from '../../components/common/header/Header'
+import TabOptions from '../../components/common/tabOptions/TabOptions'
+import Shop from '../shop/Shop'
+import Login from '../contact/Contact'
+import AboutUs from '../../components/aboutUS/AboutUs'
+import ExploreSection from '../../components/common/exploreSection/ExploreSection'
+
+const Home = () => {
+
+  const [activeTab, setActiveTab] = useState<string>('Shop');
+
+  const getCorrectScreen = (tab: string) => {
+    switch (tab) {
+      case "Shop":
+        return <Shop />
+      case "Book":
+        return <Login/>
+      case "Config":
+        return <AboutUs />
+      default:
+        return <Shop />
+    }
+  }
+
+  return (<>
+    <Header />
+    <TabOptions activeTab={activeTab} setActiveTab={setActiveTab} />
+    {getCorrectScreen(activeTab)}
+    <Footer />
+  </>
+  )
+}
+
+
+
+export default Home
