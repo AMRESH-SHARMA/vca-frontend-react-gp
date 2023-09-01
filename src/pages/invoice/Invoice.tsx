44014dbb66d5b4425c0bb44d85349143b30c3357
AMRESH-SHARMA <amreshaiio@gmail.com>
2023-09-01 18:36:05 +0530

    invoice pdf

--- src/pages/invoice/Invoice.tsx
+++ src/pages/invoice/Invoice.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { CURRENT_BASE_URL } from '../../utility/apiConfig'
 import Navbar from '../../components/common/navbar/Navbar'
+import { downloadPdfDocument } from '../../utility/util'
 
 interface ModelData {
   id: number,
@@ -16,9 +17,17 @@ interface ModelData {
   updatedAt: string
 }
 
+interface UserData {
+  company_name: string | null;
+  telephone: string;
+  gst_number: string;
+  username: string;
+}
+
 const Invoice = () => {
 
   const { modelId } = useParams()
+  const [userData, setUserData] = useState<UserData>();
   const [selectedData, setSelectedData] = useState<any[]>([]);
   const [totalCompAmt, setTotalCompAmt] = useState<number>();
   const [modelData, setModelData] = useState<ModelData>()
@@ -33,22 +42,12 @@ const Invoice = () => {
       setSelectedData(JSON.parse(dataFromStorage));
     }
 
-    const totalAmount = selectedData.reduce((acc, item) => {
-      const pricePart = item.selectedValue.split('@')[4];
-      const price = parseFloat(pricePart);
-
-      if (!isNaN(price)) {
-        return acc + price;
-      }
-
-      return acc;
-    }, 0);
-
-    setTotalCompAmt(totalAmount)
-
     try {
       (async () => {
         let res = await axios.get(`/models/${modelId}`)
+        let userInfo = await axios.get(`/auth/user/${localStorage.getItem('userId')}`)
+        console.log(userInfo)
+        setUserData(userInfo.data.data[0])
         setModelData(res.data.data.models)
         setMinQuantity(res.data.data.models.minQty)
         // console.log(res.data.data.models)
@@ -57,6 +56,14 @@ const Invoice = () => {
       console.log(e)
     }
   }, [])
+
+  useEffect(() => {
+    const totalPrice = selectedData.reduce((acc, item) => {
+      return acc + parseInt(item.selectedValue.split('@')[3], 10);
+    }, 0);
+    // console.log('tp', parseInt(totalPrice, 10))
+    setTotalCompAmt(totalPrice)
+  }, [selectedData])
 
   const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -73,6 +80,7 @@ const Invoice = () => {
   };
 
   const handlePay = () => {
+    downloadPdfDocument()
     // console.log('pay');
     // console.log("userId", 1);
     // console.log(modelId);
@@ -89,13 +97,14 @@ const Invoice = () => {
       axios.post('/invoice', requestData)
         .then((res) => {
           console.log('API response:', res);
-          if (res.data.status == 200) { console.log(res); alert("Ordered successfully!") }
+          if (res.data.status == 200) {
+            console.log(res); alert("Ordered successfully!")
+          }
         })
         .catch((e) => {
           console.error('API error:', e);
         })
 
-      return
     }
 
     selectedData.forEach((item) => {
@@ -122,67 +131,86 @@ const Invoice = () => {
   return (<>
     <Navbar />
     {/* {sessionStorage.getItem('selectedComp')} */}
-    <div className="details-wrapper">
+    <div className="details-wrapper" id='invoiceId'>
       <div className="details-info">
         <div className='details-info-body'>
-          <h3>Configured Features Added</h3>
+          <p>Date: <span>{new Date().toLocaleDateString()}</span></p>
+          <p>Time: <span>{new Date().toLocaleTimeString()}</span></p>
+          <p>Vehicle Configurator App</p><br/>
 
+          <h3>Configured Features Added</h3>
+          ---------------------------------------------------------------------------
           {selectedData.map((item, index) => (
             <p key={index}>
-              {item.selectedValue.split('@')[2]}
-              {item.selectedValue ? "₹" : null}<br />
+              {item.selectedValue.split('@')[2]}<br />
+              {item.selectedValue ? "₹" : null}
               {item.selectedValue.split('@')[3]}
             </p>
           ))}
+
         </div>
-        <hr />
-        <div className='details-info-body'>
-          <h3>Total</h3>
-
-          <p>Component Total: {totalCompAmt}</p>
-          <p>
-            Car Price: {modelData?.price} * {quantity || minQuantity}
-          </p>
-          <p>
-            Total Car Price: {(modelData?.price || minQuantity) * (quantity || minQuantity)}
-          </p>
-        </div>
-
-      </div>
-
-      <div className="image-wrapper">
-        <div className='image-wrapper-child'>
-          <h1>{modelData?.modName}</h1>
-          <img
-            className="details-car-img"
-            src={`${CURRENT_BASE_URL}/${modelData?.image_path}`}
-            alt="image"
-          />
-        </div>
-
+        <br />
         <div>
-          <p>Price: {modelData?.price}</p><br />
-          <p>safetyRating: {modelData?.safetyRating}</p><br />
-          <label htmlFor="quantity">Quantity:</label>
-          <input
-            type="number"
-            id="quantity"
-            name="quantity"
-            value={quantity || minQuantity}
-            onChange={handleQuantityChange}
-            className='g-input'
-          />
-          {error && <div style={{ color: 'red' }}>{error}</div>}
+          <h3>Total</h3>
+          <pre>
+            ---------------------------------------------------------------------------
+            <p>Component Total: {totalCompAmt}</p>
+            <p>
+              Car Price:       {modelData?.price} * {quantity || minQuantity}
+            </p>
+            <p>
+              Car Quantity:    {quantity || minQuantity}
+            </p>
+            <p>
+              Total Car Price: {(modelData?.price || minQuantity) * (quantity || minQuantity)}
+            </p>
+          </pre>
         </div>
+        <br />
+        <div>
+          <h3>Customer Info</h3>
+          <pre>
+            ---------------------------------------------------------------------------
+            <p>Name:      {userData?.username}</p>
+            <p>Company:   {userData?.company_name}</p>
+            <p>Telephone: {userData?.telephone}</p>
+            <p>GST NO:    {userData?.gst_number}</p>
+          </pre>
+        </div>
+
       </div>
+      <div id='rid'>
+        <div className="image-wrapper" >
+          <div className='image-wrapper-child'>
+            <h1>{modelData?.modName}</h1>
+            <img
+              className="details-car-img"
+              src={`${CURRENT_BASE_URL}/${modelData?.image_path}`}
+              alt="image"
+            />
+          </div>
 
+          <div id='rid'>
+            <p>Price: {modelData?.price}</p><br />
+            <p>safetyRating: {modelData?.safetyRating}</p><br />
+            <label htmlFor="quantity">Quantity:</label>
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              value={quantity || minQuantity}
+              onChange={handleQuantityChange}
+              className='g-input'
+            />
+            {error && <div style={{ color: 'red' }}>{error}</div>}
+          </div>
+        </div>
 
-
-      <div className="btn-panel">
-        <button className='button' onClick={handlePay}>Pay</button>
-      </div>
+        <div className="btn-panel">
+          <button className='button' onClick={handlePay}>Pay</button>
+        </div>
+      </div >
     </div>
-
   </>
   )
 }
